5599d6623e4b1d5110d32660516905df5859ea84
darren88881 <darren88881@outlook.com>
2020-08-01 10:44:05 +0800

    提交TestJava类

---
+++ src/com/company/TestJava.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.security.ProtectionDomain;
+
+/**
+ * @author darren
+ * @create 2020-08-01 10:33
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("上传github上");
+    }
+}
